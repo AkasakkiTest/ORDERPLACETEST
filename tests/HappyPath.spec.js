ba1548367557ef40b07cd7a00c3f9ac8040a4fd1
AkasakkiTest <zebabaig55@gmail.com>
2025-06-11 00:20:43 +0530

    TEST Commit 2

--- tests/HappyPath.spec.js
+++ tests/HappyPath.spec.js
@@ -36,35 +36,15 @@ test.only ('TEST orderplace',async({browser})=>{
     await page.waitForTimeout(5000);
 
 
-    // await page.locator("#login-form-email").fill('test002@gmail.com')
-    // await page.locator("#login-form-password").fill("Makhdoom12#")
-    // await page.locator(".login-columns button").click()
-    // await page.locator(".card-title").first().waitFor({state:"visible"})
-
-
 // PLP 
 for (let i = 0; i<= 3; i++ ){
     await PDPPage.Goto();
     await PDPPage.Addtocart();
-
-
-    //await page.goto("https://dev-sfcc.uniformadvantage.com/hypothesis-s-r-shift-ready-men-s-4-pocket-v-neck-scrub-top/HC4582.html?dwvar_HC4582_color=NAVY&dwvar_HC4582_size=L");
-    // await page.locator('.add-to-cart').click();
-    // await page.locator('[class="product-number-label"]').last().waitFor({state:'visible'});
-    
-    // await page.locator('.add-to-cart-modal-action [href="https://dev-sfcc.uniformadvantage.com/cart/"]').last().click();
-
-    // await page.locator(".line-item-style").first().waitFor({state:"visible"})
-
-// Cart,Checkout and order confirmation page
     
     await checkout.Checkoutpage();
 }
     
-    // await page.locator(".checkout-btn").click()
-    // await page.locator(".express-place-order").click()
-    // await page.locator(".order-confirmation-title").waitFor({state:"visible"})
-
+    
 
 
 })
